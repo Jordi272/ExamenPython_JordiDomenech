80e000462c9958bb89b90f6996345b6ba3a0011b
Jordi272 <jordidomenech27@gmail.com>
2023-03-22 10:45:42 +0100

    commit_3

--- functions.py
+++ functions.py
@@ -6,18 +6,27 @@ def read_data(fichero):
     with open(fichero, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            print(row[0])
+            
+            print(row.index)
+
             dicc["dato"+ str(contador)]={
                 'type' : row[0],
                 'fixed acidity':row[1],
                 'volatile acidity': row[2],
-                'residual sugar': row[3],
-                'chlorides': row[4],
-                'free sulfur'
+                'citric acid':row[3],
+                'residual sugar': row[4],
+                'chlorides': row[5],
+                'free sulfur': row[6],
+                'totl sulfur dioxide': row[7],
+                'density': row[8],
+                'PH': row[9],
+                'sulphates': row[10],
+                'alcohol':row[11],
+                'quality':row[12]
 
             }
             contador+=1
 
-    #print(dicc)
+    
 
 read_data(fichero)
